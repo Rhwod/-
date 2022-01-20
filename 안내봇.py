ef1d7e4d521eee6793cb32d877faa30252f58e82
DESKTOP-RSLGKI7\SMART <yanglab2018@gmail.com>
2022-01-20 09:43:27 +0900

    안내봇 수정

--- 안내봇.py
+++ 안내봇.py
@@ -1,4 +1,5 @@
 from http import server
+from lib2to3.pgen2.token import AWAIT
 from math import fabs
 import numbers
 import discord, asyncio, datetime, pytz
@@ -17,7 +18,23 @@ async def on_message(message):
         await message.author.send ("{}님 안녕하세요! 안내봇이에요! 이 서버에 대해 궁금한게 있으면 뭐든지 저에게 물어보세요!".format(message.author.mention, message.author))
     
     if message.content == "안내야 서버 인원 수":
-        await message.channel.send ("지금 {ctx.guild.name} 서버에는 22명의 사용자가 있어요!")
+        server_name = "`5학년 6반 수다수다`"
+        await message.channel.send ("지금 {} 서버에는 22명의 사용자가 있어요!".format(server_name))
+
+    if message.content == "안내야 멈춰!":
+        await message.channel.send ("{}님도 멈취!".format(message.author.mention))
+
+    if message.content == "안내야 바보":
+        await message.channel.send ("안내는 바보 아니거든요!")
+
+    if message.content == "안내야 샌즈":
+        await message.channel.send ("와! 샌즈! 언더태일! 아시는구나! 참고로 그거 겁나 어렵습니다")
+
+    if message.content == "안내야 안내는 누가 만들었어?":
+        await message.channel.send ("`꼬쟁 (양하율)#1582` 님이 만들어주셨어요!")
+    
+    if message.content == "안내야 안녕":
+        await message.channel.send ("{}님 안녕하세요!".format(message.author.mention))
 
 
 # 그냥 토큰이다
