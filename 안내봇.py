58f235619ab476feb9f2fc2e9c7ecfc84895c4be
DESKTOP-RSLGKI7\SMART <yanglab2018@gmail.com>
2022-01-20 09:20:17 +0900

    TOKEN 삭제

--- 안내봇.py
+++ 안내봇.py
@@ -1,3 +1,4 @@
+from http import server
 from math import fabs
 import numbers
 import discord, asyncio, datetime, pytz
@@ -15,13 +16,11 @@ async def on_message(message):
         await message.channel.send ("{}, DM으로 안내를 전송했어요! 확인해보세요!".format(message.author.mention))
         await message.author.send ("{}님 안녕하세요! 안내봇이에요! 이 서버에 대해 궁금한게 있으면 뭐든지 저에게 물어보세요!".format(message.author.mention, message.author))
     
-async def on_message(message):
     if message.content == "안내야 서버 인원 수":
-        await message.channel.send ("현재 이 서버의 인원 수는... 22명이에요!")
-        
+        await message.channel.send ("지금 {ctx.guild.name} 서버에는 22명의 사용자가 있어요!")
 
 
 # 그냥 토큰이다
-client.run('ODk4NDQxNjQ5ODc1NzM0NTQw.YWkQ_Q.-yNP02y4qNds44z7veLE9cf9t-A')
+client.run('')
 
 # 근데 굳이 내 깃헙에 와서 이걸 읽는 이상한 사람이 있진 않겠지?
